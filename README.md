7685a655bbe4085e7436a6d4eb89c03fab68a02d
krizsan <ivan.krizsan@gmail.com>
2016-02-08 16:37:43 +0100

    Switched base image to Alpine Linux with Python.

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 Docker image with Elastalert on Ubuntu.
 
 Requires a link to a Docker container running Elasticsearch using the "elasticsearch_host" alias.
-Assumes the use of port 9200 when communicating with Elasticsearch.
+Assumes the use of port 9200 when communicating with Elasticsearch.<br/>
+In order for the time of the container to be synchronized (ntpd), it must be run with the SYS_TIME capability.
+In addition you may want to add the SYS_NICE capability, in order for ntpd to be able to modify its priority.
 
 # Volumes
 /opt/logs       - Elastalert and Supervisord logs will be written to this directory.<br/>
--- start-elastalert.sh
+++ start-elastalert.sh
@@ -4,16 +4,16 @@ set -e
 
 # Set the timezone.
 if [ "$SET_CONTAINER_TIMEZONE" = "true" ]; then
-	echo ${CONTAINER_TIMEZONE} >/etc/timezone && \
-	dpkg-reconfigure -f noninteractive tzdata
+	setup-timezone -z ${CONTAINER_TIMEZONE} && \
 	echo "Container timezone set to: $CONTAINER_TIMEZONE"
 else
 	echo "Container timezone not modified"
 fi
 
 # Force immediate synchronisation of the time and start the time-synchronization service.
-ntpd -gq
-service ntp start
+# In order to be able to use ntpd in the container, it must be run with the SYS_TIME capability.
+# In addition you may want to add the SYS_NICE capability, in order for ntpd to be able to modify its priority.
+ntpd -s
 
 # Wait until Elasticsearch is online since otherwise Elastalert will fail.
 rm -f garbage_file
