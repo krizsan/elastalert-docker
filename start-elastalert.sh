e9715371eaa96f5e051717b9d9ee75c107989c5b
JamesJJ <JamesJJ@users.noreply.github.com>
2017-05-29 15:39:03 +0800

    Avoid writing to the filesystem
    Signed-off-by: JamesJJ <JamesJJ@users.noreply.github.com>

--- start-elastalert.sh
+++ start-elastalert.sh
@@ -16,25 +16,21 @@ fi
 ntpd -s
 
 # Wait until Elasticsearch is online since otherwise Elastalert will fail.
-rm -f garbage_file
-while ! wget -O garbage_file "${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}" 2>/dev/null
+while ! wget -q -T 3 -O - "${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}" 2>/dev/null
 do
 	echo "Waiting for Elasticsearch..."
-	rm -f garbage_file
 	sleep 1
 done
-rm -f garbage_file
 sleep 5
 
 # Check if the Elastalert index exists in Elasticsearch and create it if it does not.
-if ! wget -O garbage_file "${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}/elastalert_status" 2>/dev/null
+if ! wget -q -T 3 -O - "${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}/elastalert_status" 2>/dev/null
 then
-	echo "Creating Elastalert index in Elasticsearch..."
+    echo "Creating Elastalert index in Elasticsearch..."
     elastalert-create-index --host "${ELASTICSEARCH_HOST}" --port "${ELASTICSEARCH_PORT}" --config "${ELASTALERT_CONFIG}" --index elastalert_status --old-index ""
 else
     echo "Elastalert index already exists in Elasticsearch."
 fi
-rm -f garbage_file
 
 echo "Starting Elastalert..."
 exec supervisord -c "${ELASTALERT_SUPERVISOR_CONF}" -n
