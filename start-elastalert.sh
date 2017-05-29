c703eb67938330ef6ef6f12bff91b3a5fb4aae9a
JamesJJ <JamesJJ@users.noreply.github.com>
2017-05-29 15:33:06 +0800

    Fix shell quoting in start-elastalert.sh
    Signed-off-by: JamesJJ <JamesJJ@users.noreply.github.com>

--- start-elastalert.sh
+++ start-elastalert.sh
@@ -17,7 +17,7 @@ ntpd -s
 
 # Wait until Elasticsearch is online since otherwise Elastalert will fail.
 rm -f garbage_file
-while ! wget -O garbage_file ${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT} 2>/dev/null
+while ! wget -O garbage_file "${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}" 2>/dev/null
 do
 	echo "Waiting for Elasticsearch..."
 	rm -f garbage_file
@@ -27,14 +27,14 @@ rm -f garbage_file
 sleep 5
 
 # Check if the Elastalert index exists in Elasticsearch and create it if it does not.
-if ! wget -O garbage_file ${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}/elastalert_status 2>/dev/null
+if ! wget -O garbage_file "${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}/elastalert_status" 2>/dev/null
 then
 	echo "Creating Elastalert index in Elasticsearch..."
-    elastalert-create-index --host ${ELASTICSEARCH_HOST} --port ${ELASTICSEARCH_PORT} --config ${ELASTALERT_CONFIG} --index elastalert_status --old-index ""
+    elastalert-create-index --host "${ELASTICSEARCH_HOST}" --port "${ELASTICSEARCH_PORT}" --config "${ELASTALERT_CONFIG}" --index elastalert_status --old-index ""
 else
     echo "Elastalert index already exists in Elasticsearch."
 fi
 rm -f garbage_file
 
 echo "Starting Elastalert..."
-exec supervisord -c ${ELASTALERT_SUPERVISOR_CONF} -n
+exec supervisord -c "${ELASTALERT_SUPERVISOR_CONF}" -n
